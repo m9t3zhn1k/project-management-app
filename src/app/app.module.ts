930103229ef504f8aaf528e2ddc0cffdeaf0de6b
Greg <martinstudyweb@gmail.com>
2022-11-10 03:54:43 +0400

    feat: add header footer without adaptive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule, SvgIconRegistryService } from 'angular-svg-icon';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule, CoreModule],
+  imports: [BrowserModule, RouterModule, CoreModule, HttpClientModule, AngularSvgIconModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private iconReg: SvgIconRegistryService) {
+    this.iconReg.loadSvg('../assets/icons/logo.svg', 'logo')?.subscribe();
+    this.iconReg.loadSvg('../assets/icons/rs-logo.svg', 'rs-logo')?.subscribe();
+  }
+}
