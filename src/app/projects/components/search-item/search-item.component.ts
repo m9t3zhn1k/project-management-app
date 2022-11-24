5c78c215544176fad227d04a3fc8d1e19370a0da
m9t3zhn1k <m9t3zhn1k@mail.ru>
2022-11-25 00:29:39 +0300

    feat: add beauty effect of opening confirmational modal

--- src/app/projects/components/search-item/search-item.component.ts
+++ src/app/projects/components/search-item/search-item.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTimeValue } from '@constants/constants';
 
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent {}
+export class SearchItemComponent implements AfterViewInit {
+  @ViewChild('searchInput', { static: false }) searchInput: ElementRef | undefined;
+
+  @ViewChild('searchForm', { static: false }) searchForm: ElementRef | undefined;
+
+  @Output() search = new EventEmitter<string>();
+
+  ngAfterViewInit(): void {
+    fromEvent<InputEvent>(this.searchInput?.nativeElement, 'input')
+      .pipe(
+        map((event: InputEvent): string => (event.target as HTMLInputElement).value.trim().toLowerCase()),
+        map((value: string): string => (value.length >= 3 ? value : '')),
+        debounceTime(debounceTimeValue),
+        distinctUntilChanged(),
+      )
+      .subscribe((value: string): void => this.search.emit(value));
+  }
+}
--- src/app/shared/confirmation-modal/confirmation-modal.component.ts
+++ src/app/shared/confirmation-modal/confirmation-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { ConfirmationService } from './confirmation.service';
 
 @Component({
@@ -6,11 +6,19 @@ import { ConfirmationService } from './confirmation.service';
   templateUrl: './confirmation-modal.component.html',
   styleUrls: ['./confirmation-modal.component.scss'],
 })
-export class ConfirmationModalComponent {
-  constructor(public confirmationService: ConfirmationService) {}
+export class ConfirmationModalComponent implements AfterViewInit {
+  @ViewChild('modal', { static: false }) modal: ElementRef | undefined;
+
+  question: string = 'Are you sure you want to perform this action?';
+
+  constructor(public confirmationService: ConfirmationService, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    setTimeout((): void => this.renderer.addClass(this.modal?.nativeElement, 'show'), 0);
+  }
 
   closeModal(event: Event): void {
-    if ((event.target as HTMLElement).dataset.closeTarget) {
+    if ((event.currentTarget as HTMLElement).dataset.closeTarget) {
       event.stopPropagation();
       this.confirmationService.closeModal();
     }
