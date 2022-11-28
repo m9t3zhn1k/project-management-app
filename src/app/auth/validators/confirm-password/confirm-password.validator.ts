c5965bf1ba078f165dc5f15690acb6efde554464
m9t3zhn1k <m9t3zhn1k@mail.ru>
2022-11-29 00:04:27 +0300

    fix: fix budgets

--- src/app/auth/validators/confirm-password/confirm-password.validator.ts
+++ src/app/auth/validators/confirm-password/confirm-password.validator.ts
@@ -3,5 +3,7 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 export function confirmPasswordValidator(group: AbstractControl<string>): ValidationErrors | null {
   const password: string = group.get('password')?.value;
   const passwordConfirm: string = group.get('passwordConfirm')?.value;
-  return password === passwordConfirm ? { error: 'This password does not match that entered in the password field' } : null;
+  return password === passwordConfirm
+    ? { error: 'This password does not match that entered in the password field' }
+    : null;
 }
